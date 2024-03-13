7a5341ec09c254a84483de0c9694a086e5ac71b1
Scarlettszsz <125333428+Scarlettszsz@users.noreply.github.com>
2024-03-14 00:40:23 +0900

    모달 스크롤 이벤트 방지 및 css 수정

--- resources/assets/js/common.js
+++ resources/assets/js/common.js
@@ -384,18 +384,25 @@ $(document).ready(function () {
   });
 
   // -------------- COMMON --------------
-
+  function removeDefaultEvent(event) {
+    event.preventDefault();
+  }
   // 모달 열기 버튼 클릭 이벤트
   $(".open-modal").click(function () {
     var modalId = $(this).data("modal-id");
     $("#" + modalId).addClass("active");
     // $("body").css("overflow", "hidden");
+    // 스크롤 이벤트 방지
+    window.addEventListener("wheel", removeDefaultEvent, {passive:false});
   });
+
   // 모달 닫기 버튼 및 모달 바깥 영역 클릭 이벤트
   $(".btn-modal-close, .gt-modal-wrap").click(function () {
     $(".gt-modal-wrap").removeClass("active");
-    $("body").css("overflow", "auto");
+    // $("body").css("overflow", "auto");
+    window.removeEventListener("wheel", removeDefaultEvent);
   });
+  
   // 모달 내부 클릭 시 닫기 방지
   $(".gt-modal-content").click(function (e) {
     e.stopPropagation();
